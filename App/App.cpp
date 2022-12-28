01f4937e113a84aeed794e5eb9fea26dadd1a3d0
Teodor Hristov <tedotedo771@gmail.com>
2022-12-28 17:31:19 +0200

    Add app files.

---
+++ App/App.cpp
@@ -0,0 +1,19 @@
+﻿// LZ78.cpp : Defines the entry point for the application.
+//
+
+#include "App.h"
+#include "Compressor.hpp"
+
+using namespace std;
+
+int main()
+{
+	Compressor c;
+	vector<int> a = c.compress("ALABALAPORTOKALA");
+	cout << "ZIP: ";
+	for(auto e :a)
+		cout << e << " ";
+	cout << endl;
+	cout << "UNZIP: " << c.decompress(a);
+	return 0;
+}
