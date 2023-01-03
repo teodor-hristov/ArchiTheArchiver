ac2ba58c98b8f6b0def9a65959d3255bfde725cf
Teodor Hristov <tedotedo771@gmail.com>
2023-01-03 23:07:31 +0200

    Add skeleton for parser namespace.

---
+++ App/Parser.cpp
@@ -0,0 +1,16 @@
+#include "Parser.hpp"
+
+bool parser::zip(std::vector<std::string>& files)
+{
+	return false;
+}
+
+bool parser::unzip(std::string& filePath)
+{
+	return false;
+}
+
+void parser::parse(std::istream& in, std::ostream& out)
+{
+	return;
+}
---
+++ App/Parser.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+namespace parser
+{
+	bool zip(std::vector<std::string>& files);
+	bool unzip(std::string& filePath);
+	void parse(std::istream& in, std::ostream& out);
+}
