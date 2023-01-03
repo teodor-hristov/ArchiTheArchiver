c8417b7858c1ae68d8ae08a13d8f4ed3d365e138
Teodor Hristov <tedotedo771@gmail.com>
2023-01-04 01:00:23 +0200

    The concept of the archive header is now deprecated.

--- App/Utils.hpp
+++ App/Utils.hpp
@@ -3,17 +3,12 @@
 #include <vector>
 #include <string>
 
-struct FileHeader
-	{
-		double compression_level;
-		size_t data_size;
-		std::string file_name;
-		//uint32_t file_hash;
-	};
 struct FileEntry
 	{
-		FileHeader header;
-		std::vector<int> data;
+		size_t data_size_compressed; //in bytes
+		size_t data_size_raw; //in bytes
+		char file_name[32];
+		//uint32_t file_hash;
 
-		FileEntry(std::string fileName);
+		FileEntry(std::string& fileName);
 };
