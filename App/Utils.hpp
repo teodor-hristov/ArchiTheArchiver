1cb34977d33a95093e59212b49190b7bb4f2f205
Teodor Hristov <tedotedo771@gmail.com>
2023-01-08 17:59:05 +0200

    Resize file_name array.

--- App/Utils.hpp
+++ App/Utils.hpp
@@ -7,8 +7,8 @@ struct FileEntry
 	{
 		size_t data_size_compressed; //in bytes
 		size_t data_size_raw; //in bytes
-		char file_name[32];
+		char file_name[256];
 		//uint32_t file_hash;
 
-		FileEntry(std::string& fileName);
+		FileEntry(const std::string fileName);
 };
